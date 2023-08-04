6921dc58cc0d0a9ba115d034dc160c8615613ba8
Iamneo <techadmin@iamneo.ai>
2023-08-04 15:06:20 +0000

    Backup Commit

--- facebook_testing/src/test/java/com/qa/pages/FacebookPage.java
+++ facebook_testing/src/test/java/com/qa/pages/FacebookPage.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class Fb_Pages {
+public class FacebookPage {
     private final WebDriver driver;
 
     // Locators
--- facebook_testing/src/test/java/com/qa/testscripts/Fb_001.java
+++ facebook_testing/src/test/java/com/qa/testscripts/Fb_001.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import com.qa.pages;
 
 public class Fb_001 {
 
